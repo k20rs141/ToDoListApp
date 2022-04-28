8d0226ef301437155099ecfdca9779f0d36be2aa
Kouki <yamadakoki0124@icloud.com>
2022-04-28 22:12:43 +0900

    Initial Commit

---
+++ toDoListApp/toDoListAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  toDoListAppApp.swift
+//  toDoListApp
+//
+//  Created by 山田滉暁 on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct toDoListAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
