7d5a7e19f5f822f3d005164213aa19c0276aadab
Kouki <yamadakoki0124@icloud.com>
2022-05-06 22:41:53 +0900

    ListからFormに変更

--- toDoListApp/Views/SettingView.swift
+++ toDoListApp/Views/SettingView.swift
@@ -14,60 +14,44 @@ struct SettingView: View {
     var body: some View {
         VStack(spacing: 0) {
             
-            VStack {
-                Text("設定")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-            }
-            .frame(maxWidth: .infinity, maxHeight: 60, alignment: .leading)
-            .padding(.top, 50)
-            .padding(.horizontal, 30)
-            
             NavigationView {
-
-                VStack {
                     
-                    List {
-                       
-                        Section {
-                            Text("アカウント")
-                            Text("通知")
-                        } header: {
-                            Text("ユーザー")
-                        }
-                        .foregroundColor(.primary)
-                        
-                        Section {
-                            Text("利用規約")
-                            Text("プライバシーポリシー")
-                            Text("お問い合わせ")
-                            Text("FAQ")
-                        } header: {
-                            Text("アプリ情報")
-                        }
-                        .foregroundColor(.primary)
-                        
-                        Section {
-                            Text("文字サイズ")
-                            Text("リスト色")
-                            
-                            Toggle(isOn: $nightMode) {
-                                Text("ナイトモード")
-                            }
-                        } header: {
-                            Text("テーマ・カラー")
-                        }
-                        .foregroundColor(.primary)
+                Form {
+                   
+                    Section {
+                        Text("アカウント")
+                        Text("通知")
+                    } header: {
+                        Text("ユーザー")
                     }
-                    .listStyle(.insetGrouped)
+                    .foregroundColor(.primary)
+                    
+                    Section {
+                        Text("利用規約")
+                        Text("プライバシーポリシー")
+                        Text("お問い合わせ")
+                        Text("FAQ")
+                    } header: {
+                        Text("アプリ情報")
+                    }
+                    .foregroundColor(.primary)
+                    
+                    Section {
+                        Text("文字サイズ")
+                        Text("リスト色")
+                        
+                        Toggle(isOn: $nightMode) {
+                            Text("ナイトモード")
+                        }
+                    } header: {
+                        Text("テーマ・カラー")
+                    }
+                    .foregroundColor(.primary)
                 }
-                .frame(height: 750)
+                .navigationTitle("設定")
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-        .background(
-            Color(red: 0.955, green: 0.955, blue: 0.955)
-        )
     }
 }
 
